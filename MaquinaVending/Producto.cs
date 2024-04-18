a2bc31c9654700f6acbbd84bf43f489251e2dace
Paulagove <paulagon27@gmail.com>
2024-04-18 10:08:08 +0200

    inicio

---
+++ MaquinaVending/Producto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaquinaVending {
+    internal abstract class Producto {
+    protected string Nombre {  get; set; }
+    protected int Unidades { get; set; }
+    protected double PrecioUnitario { get; set; }
+    protected string Descripcion { get; set; }
+    
+    }
+}
